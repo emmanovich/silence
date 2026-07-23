eefd782c4b547b97759d031c3737c06de3b47ad5
emmanovich <emmanovich@ukr.net>
2026-07-23 18:30:44 +0300

    Add Formatter class with show method

---
+++ formatter.py
@@ -0,0 +1,35 @@
+class Formatter:
+
+    def show(
+
+        self,
+
+        source,
+
+        calendars
+
+    ):
+
+        print()
+
+        print("Input Date\n")
+
+        print(source)
+
+        print()
+
+        for item in calendars:
+
+            print(
+
+                item.calendar
+
+            )
+
+            print(
+
+                item.value
+
+            )
+
+            print()
