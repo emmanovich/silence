db76c6bb5654285f656adb4206394bdce51e3d70
emmanovich <emmanovich@ukr.net>
2026-07-23 18:30:24 +0300

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,19 @@
+from calendars import CalendarLibrary
+
+class Converter:
+
+    def build(
+
+        self,
+
+        date
+
+    ):
+
+        library = CalendarLibrary()
+
+        return library.convert(
+
+            date
+
+        )
